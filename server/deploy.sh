6eaa17cb6b2b2f11a2a0abd89e12d06d75f8985c
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2015-07-20 20:43:02 +0100

    adds docker

--- server/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get last version
-git checkout .;
-git pull;
-
-# Restart server
-sudo forever stop phonepad;
-sudo forever --uid "phonepad" -a -l phonepad.log -o phonepad.log -e phonepad.log start app.js;
